dbd9cc0ef86823cfdf9ab0badec0de81e400c035
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-03-17 10:51:27 +0530

    Time: 3 ms (30.38%), Space: 6.2 MB (8.12%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+       int n=s.length();
+        
+        stack<int>st;
+        
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+           if(s[i]=='('){
+               st.push(count);
+               count=0;
+               
+           }else {
+              count=st.top()+max(2*count,1);
+              st.pop();
+           }
+        }
+        
+        return count;
+    }
+};
