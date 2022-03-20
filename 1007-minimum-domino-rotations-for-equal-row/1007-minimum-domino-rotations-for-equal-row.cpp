3e5b02719a0b5f85e3a141224c12176a1c4d77cd
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-03-20 23:47:44 +0530

    Time: 173 ms (61.64%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,77 +1,34 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+     
+        int n=tops.size();
+        int top=tops[0];
+      int bot=bottoms[0];
         
-        unordered_map<int,int>map;
-        int top=0;
-        int count=0;
+        int top1=0;
+        int top2=0;
+        int bot1=0;
+        int bot2=0;
         
         for(int i=0;i<tops.size();i++){
-            map[tops[i]]++;
-                 
-            if(count<map[tops[i]]){
-                count=map[tops[i]];
-                top=tops[i];
+            if(tops[i]!=top && bottoms[i]!=top){
+                top=0;
+            }
+            
+            if(bottoms[i]!=bot && tops[i]!=bot){
+                bot=0;
             }
             
             
+            top1+=tops[i]==top;
+            bot1+=bottoms[i]==top;
+            top2+=tops[i]==bot;
+            bot2+=bottoms[i]==bot;
             
         }
         
-        map.clear();
-        
-        int bottom=0;
-        count=0;
-        for(int i=0;i<bottoms.size();i++){
-            map[bottoms[i]]++;
-                 
-            if(count<map[bottoms[i]]){
-                count=map[bottoms[i]];
-                bottom=bottoms[i];
-            }
-        }
-        bool check1=true;
-        int ans1=0;
-        for(int i=0;i<tops.size();i++){
-            if(tops[i]==top){
-                continue;
-            }
-            
-            else if(tops[i]!=top && bottoms[i]!=top){
-                check1=false;
-                break;
-            }else{
-                ans1++;
-            }
-        }
-        
-        
-        
-        bool check2=true;
-        int ans2=0;
-        for(int i=0;i<bottoms.size();i++){
-            if(bottoms[i]==bottom){
-                continue;
-            }
-            else if(bottoms[i]!=bottom && tops[i]!=bottom){
-                check2=false;
-                break;
-            }else{
-                ans2++;
-            }
-        }
-        
-        if(check1 && check2){
-            return min(ans1,ans2);
-        }
-        else if(!check1 && check2){
-            return ans2;
-        }else if(check1 && !check2){
-            return ans1;
-        }
-        else{
-            return -1;
-        }
+        return top||bot ? min(n-max(top1,bot1),n-max(top2,bot2)) : -1;
     
     }
 };
