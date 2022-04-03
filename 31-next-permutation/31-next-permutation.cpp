8a16f522a62c0fe008cb6f06e09948e88749c4e0
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-04-03 23:35:09 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        
         int n=nums.size();
         
-        if(nums.size()==0 || nums.size()==1){
-            return;
+        if(n==1){
+            return ;
         }
         
         
         if(nums[n-1]>nums[n-2]){
-            int temp=nums[n-2];
-            nums[n-2]=nums[n-1];
-            nums[n-1]=temp;
+            swap(nums[n-1],nums[n-2]);
             return ;
         }
-        
-        if(n==2){
-            sort(nums.begin(),nums.end());
-            return;
-        }
-        
+            
         int i=n-2;
         int j=n-3;
         
         while(j>=0){
-          if(nums[j]>=nums[i]){
-             i--;
-             j--;
-              
-          }else{
-              int num=nums[j];
-              sort(nums.begin()+j+1,nums.end());
-              
-              int k=j+1;
-              while(nums[k]<=num && k!=n){
-                  k++;
-              }
-              swap(nums[k],nums[j]);
-              
-              sort(nums.begin()+i,nums.end());
-              return ;
-              
-          }   
+            if(nums[i]<=nums[j]){
+                i--;
+                j--;
+                
+            }else{
             
-        }
+            int num=nums[j];
+            sort(nums.begin()+i,nums.end());
+            
+            int k=i;
+                while(num>=nums[k] && k!=n){
+                    k++;
+                }
+            
+            
+            swap(nums[j],nums[k]);
+            sort(nums.begin()+i,nums.end());
+            
+            return ;
+            
+          }
+            
+      }     
+        sort(nums.begin(),nums.end());
         
-         sort(nums.begin(),nums.end());
-
-         return ;
+        return ;
+        
         
     }
 };
