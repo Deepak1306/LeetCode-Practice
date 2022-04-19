98ce14918ea3af4d3577cd7ac6f62d81c0a94c9d
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-04-19 17:46:35 +0530

    Time: 11 ms (68.67%), Space: 14 MB (50.99%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     
     
-    void helper(vector<vector<int>>& image, int sr, int sc, int newColor,int n,int m, int source){
+    void helper(vector<vector<int>>& image, int sr, int sc, int newColor,int n,int m, int source, vector<vector<bool>>&visited ){
       if(sr<0 || sr>=n || sc<0 || sc>=m ){
           return ;
-      }else if(image[sr][sc]!=source){
+      }else if(image[sr][sc]!=source || visited[sr][sc]){
           return ;
       }
         image[sr][sc]=newColor;
+        visited[sr][sc]=true;
       
-        helper(image ,sr-1,sc,newColor,n,m,source);
-        helper(image ,sr,sc-1,newColor,n,m,source);
-        helper(image ,sr,sc+1,newColor,n,m,source);
-        helper(image ,sr+1,sc,newColor,n,m,source);
+        helper(image ,sr-1,sc,newColor,n,m,source,visited);
+        helper(image ,sr,sc-1,newColor,n,m,source,visited);
+        helper(image ,sr,sc+1,newColor,n,m,source,visited);
+        helper(image ,sr+1,sc,newColor,n,m,source,visited);
         
         return;
         
@@ -22,16 +23,17 @@ public:
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
-        if(newColor==image[sr][sc]){
-            return image;
-        }
         
+       
         
         int n=image.size();
         int m=image[0].size();
         int source=image[sr][sc];
+       
+         vector<vector<bool>> visited(n,vector<bool>(m,false));
+         
         
-        helper(image,sr,sc,newColor,n,m,source);
+        helper(image,sr,sc,newColor,n,m,source,visited);
         
         return image;
         
