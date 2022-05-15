24c0aa65812192a1b8b0c84c2c3da238cd8eb27a
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-15 10:26:01 +0530

    Time: 8 ms (58.18%), Space: 6.1 MB (100.00%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        int m=s.length();
+        int n=p.length();
         
-        int n=s.length();
-        int m=p.length();
+        int dp[n+1][m+1];
         
-        bool dp[m+1][n+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            if(p[i-1]=='*'){
+                dp[i][0]=dp[i-2][0];
+            }else{
+                dp[i][0]=false;
+            }
+            
+        }
         
+        for(int j=1;j<=m;j++){
+            dp[0][j]=false;
+        }
         
-        
-        
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=true;
-                }else if(i==0){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(s[j-1]==p[i-1]){
+                   dp[i][j]=dp[i-1][j-1]; 
+                }
+                else if(p[i-1]=='.'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p[i-1]=='*'){
+                    dp[i][j]=dp[i-2][j];
+                    
+                    if(p[i-2]==s[j-1] || p[i-2]=='.'){
+                        dp[i][j]=dp[i][j]||dp[i][j-1];
+                           }
+                }else{
                     dp[i][j]=false;
                 }
-                else if(j==0){
-                    if(p[i-1]=='*'){
-                        dp[i][j]=dp[i-2][j];
-                    }else{
-                        dp[i][j]=false;
-                    }
-                }else{
-                    if(p[i-1]=='*'){
-                        dp[i][j]=dp[i-2][j];
-                        
-                        if(p[i-2]==s[j-1] || p[i-2]=='.'){
-                            dp[i][j]=dp[i][j]||dp[i][j-1];
-                        }
-                        
-                    }else if(p[i-1]=='.'){
-                        dp[i][j]=dp[i-1][j-1];
-                    }else if(p[i-1]==s[j-1]){
-                        dp[i][j]=dp[i-1][j-1];
-                    }else{
-                        dp[i][j]=false;
-                    }
-                    
-                    
-                    
-                }
+                
+                
             }
         }
         
-        return dp[m][n];
         
+        return dp[n][m];
     }
 };
