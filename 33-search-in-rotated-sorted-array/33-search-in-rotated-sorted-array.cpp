91c3c4bea396d926288cb97b2cf44b6a19df88b4
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-17 18:53:48 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (95.97%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-     
-        int n=nums.size()-1;
-        
+       int n=nums.size();
         int i=0;
-        int j=n;
+        int j=n-1;
+        
         
         while(i<=j){
             int mid=i+(j-i)/2;
-            
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]==nums[i] && nums[mid]==nums[j]){
-                i++;
-                j--;
-            }else if(nums[mid]>=nums[i]){
-                if(target>=nums[i] && target<nums[mid]){
-                    j=mid-1;
-                }else{
-                    i=mid+1;
-                }
-                
-            }else{
-                if(target<=nums[j] && target>nums[mid]){
-                    i=mid+1;
-                }else{
-                    j=mid-1;
-                }
-            }
-            
+           if(nums[mid]==target){
+               return mid;
+           }else if(nums[i]<=nums[mid]){
+               if(target>=nums[i] && target<nums[mid]){
+                   j=mid-1;
+               }else{
+                   i=mid+1;
+               }
+           }else{
+               if(target<=nums[j] && target>nums[mid]){
+                   i=mid+1;
+               }else{
+                   j=mid-1;
+               }
+           } 
+           
         }
         
         return -1;
