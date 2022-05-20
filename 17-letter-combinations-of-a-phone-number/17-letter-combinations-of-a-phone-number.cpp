e92b65e2430c1fd6595198e30572ba90bf9db431
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-20 15:44:11 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (61.54%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,33 @@
  string str[]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 class Solution {
 public:
-    void helper(vector<string>&ans,string digits,string output){
-        int n=digits.length();
+    vector<string>ans;
+    
+    void helper(string digits,string output){
+       int n=digits.length();
         
         if(n==0){
             ans.push_back(output);
             return ;
         }
         
-        
-        int a=digits[0]-'2';
+        int a=digits[0]-'0'-2;
         string s=str[a];
         
         for(int i=0;i<s.length();i++){
-            helper(ans,digits.substr(1),output+s[i]);
+            helper(digits.substr(1),output+s[i]);
         }
-        
-        return ;
         
         
     }
    
     vector<string> letterCombinations(string digits) {
-       vector<string>ans;
-        int n=digits.length();
-        if(n==0){
+        if(digits.length()==0){
             return ans;
         }
         
-        helper(ans,digits,"");
-        
+        helper(digits,"");
+       
         return ans;
-        
     }
 };
