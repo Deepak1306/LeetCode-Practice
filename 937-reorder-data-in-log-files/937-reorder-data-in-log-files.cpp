9061655357c6ecb7182b11cf10f7462efdc92e74
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-22 22:27:06 +0530

    Time: 23 ms (28.13%), Space: 15.1 MB (13.20%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,28 @@
+bool compare(string a, string b){
+        int i=a.find(' ');
+        int j=b.find(' ');
+        
+        if(!isdigit(a[i+1]) && !isdigit(b[j+1])){
+        
+           if(a.substr(i+1)==b.substr(j+1)){
+             return a.substr(0,i+1)<b.substr(0,j+1);
+           }
+            
+            return a.substr(i+1)<b.substr(j+1);
+        }else if(!isdigit(a[i+1])){
+            return true;
+        }
+        return false;
+        
+        
+    }
+    
+
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        
+        stable_sort(logs.begin(),logs.end(),compare);
+        return logs;
+    }
+};
