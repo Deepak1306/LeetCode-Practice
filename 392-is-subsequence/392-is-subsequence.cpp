239b2ea9acfe94afaa8b0b7454cd9f494cdd59d0
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-25 15:47:16 +0530

    Time: 4 ms (38.50%), Space: 7 MB (15.75%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n=t.length();
-        int k=0;
+        int n=s.length();
+        int m=t.length();
         
-        for(int i=0;i<n;i++){
-            if(k==s.length()){
-                return true;
-            }
-            
-            if(s[k]==t[i]){
-                k++;
+        if(n>m){
+            return false;
+        }    
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
         
-        if(k==s.length()){
-            return true;
+        int i=n;
+        int j=m;
+        
+        string str="";
+        
+        while(i>0 && j>0){
+            if(s[i-1]==t[j-1]){
+                 str+=s[i-1];
+                i--;
+                j--;
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                i--;
+            }else{
+                j--;
+            }
         }
         
-        return false;
+        reverse(str.begin(),str.end());
+        return s==str;
+        
     }
 };
