9c5bf08f0959e98d6fef61748e53495fe67c4bac
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-30 10:17:59 +0530

    Added solution - LeetHub

--- Count Palindromic Subsequences - GFG/count-palindromic-subsequences.cpp
+++ Count Palindromic Subsequences - GFG/count-palindromic-subsequences.cpp
@@ -7,39 +7,44 @@ using namespace std;
 class Solution{
     public:
     /*You are required to complete below method */
-    long long int  countPS(string s)
+    long long int  countPS(string A)
     {
        //Your code here
-       int n=s.length();
-       long long int mod=1e9+7;
-       long long int dp[n][n];
-       
-       for(int g=0;g<n;g++){
-           for(int i=0,j=g;j<n;j++,i++){
-               if(g==0){
-                   dp[i][j]=1;
-               }else if(g==1){
-                   if(s[i]==s[j]){
-                       dp[i][j]=3;
-                   }else{
-                       dp[i][j]=2;
-                   }
-               }else{
-                   if(s[i]==s[j]){
-                       dp[i][j]=(1+dp[i+1][j]+dp[i][j-1])%mod;
-                   }else{
-                       dp[i][j]=(dp[i+1][j]+dp[i][j-1]-dp[i+1][j-1])%mod;
-                   }
-                   
-                   if(dp[i][j]<0){
-                       dp[i][j]+=mod;
-                   }
-                   
-               }
-           }
-       }
-       
-       return dp[0][n-1]%mod;
+        long long int n=A.length();
+        
+        long long int dp[n][n];
+        
+        long long int mod=1e9+7;
+        
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;j<n;j++,i++){
+                if(g==0){
+                   dp[i][j]=1; 
+                }else if(g==1){
+                    
+                    if(A[i]==A[j]){
+                        dp[i][j]=3;
+                    }else{
+                        dp[i][j]=2;
+                    }
+                  
+                }else{
+                  if(A[i]==A[j]){
+                      dp[i][j]=(dp[i][j-1]+dp[i+1][j]+1)%mod;
+                  }else{
+                      dp[i][j]=(dp[i][j-1]+dp[i+1][j]-dp[i+1][j-1])%mod;
+                  }
+                  
+                  if(dp[i][j]<0){
+                      dp[i][j]+=mod;
+                  }
+                }
+            }
+        }
+        
+        return dp[0][n-1]%mod;
+      
+        
     }
      
 };
