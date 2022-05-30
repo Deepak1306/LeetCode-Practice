aa04b7f99b5c705d744b17a07bbec6abe8d5652e
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-05-30 21:46:10 +0530

    Added solution - LeetHub

---
+++ Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	int findWinner(int N, int x, int y)
+	{
+		// Your code goes here
+		if(x>y){
+		    swap(x,y);
+		}
+		int dp[N+1];
+		memset(dp,0,sizeof(dp));
+		
+		dp[1]=1;
+		dp[x]=1;
+		dp[y]=1;
+		
+		for(int i=2;i<=N;i++){
+		    if(i<x){
+		        dp[i]=dp[i-1]^1;
+		    }else if(i<y){
+		        dp[i]=dp[i-1]^1 || dp[i-x]^1;
+		    }else{
+		        dp[i]=dp[i-1]^1 || dp[i-x]^1 || dp[i-y]^1;
+		    }
+		}
+		
+		return dp[N];
+	}
+
+};
+	
+
+// { Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+       	int n, x, y;
+       	cin >> n >> x >> y;
+
+       
+
+        Solution ob;
+        cout << ob.findWinner(n, x, y);
+	    cout << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends
