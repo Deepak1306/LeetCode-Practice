412bae69b0047ad773de20fb9d9967dd4805f8eb
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-02 07:50:08 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (86.77%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         
-        unordered_map<string,int>map;
-        int n=s.length();
-        map[goal]=1;
         int i=0;
+        int n=s.length();
+        
+        if(s==goal){
+            return true;
+        }
         
         while(i<n){
             
-            if(map[s]!=0){
-                return true;
-            }
-            int temp=s[0];
+            char temp=s[0];
             for(int i=0;i<n-1;i++){
                 s[i]=s[i+1];
             }
             s[n-1]=temp;
+            
+            if(s==goal){
+                return true;
+            }
+            
             i++;
+            
         }
         
         return false;
