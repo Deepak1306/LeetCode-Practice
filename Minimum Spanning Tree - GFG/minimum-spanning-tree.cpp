c208e4274e75519d09a2a14f9749eb7f16c9f9b8
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-03 16:48:11 +0530

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>key(V+1,INT_MAX);
+        vector<int>mst(V+1,false);
+        
+        key[0]=0;
+        pq.push({0,0});
+        
+        while(!pq.empty()){
+            int node=pq.top().second;
+            pq.pop();
+            
+            mst[node]=true;
+            for(auto j:adj[node]){
+                int wi=j[1];
+                int next=j[0];
+                
+                if(mst[next]==false && wi<key[next]){
+                    key[next]=wi;
+                    pq.push({key[next],next});
+                }
+                
+                
+            }
+            
+            
+            
+        }
+        
+        int sum=0;
+        for(int i=0;i<V;i++){
+            if(key[i]!=INT_MAX){
+                sum+=key[i];
+            }
+            
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
