14e00158d356b6607a4295fee926b38123504b92
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-04 21:19:20 +0530

    Time: 8 ms (84.09%), Space: 14 MB (50.90%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-    
-    
-    void helper(vector<vector<int>>& image, int sr, int sc, int newColor,int n,int m, int source, vector<vector<bool>>&visited ){
-      if(sr<0 || sr>=n || sc<0 || sc>=m ){
-          return ;
-      }else if(image[sr][sc]!=source || visited[sr][sc]){
-          return ;
-      }
-        image[sr][sc]=newColor;
-        visited[sr][sc]=true;
-      
-        helper(image ,sr-1,sc,newColor,n,m,source,visited);
-        helper(image ,sr,sc-1,newColor,n,m,source,visited);
-        helper(image ,sr,sc+1,newColor,n,m,source,visited);
-        helper(image ,sr+1,sc,newColor,n,m,source,visited);
+void helper(vector<vector<int>>& image,int source,int sr, int sc, int newColor,int n,int m){
         
-        return;
         
+        if(sr<0 || sr>=n || sc<0 || sc>=m){
+            return ;
+        }
+        
+    if(image[sr][sc]==newColor || image[sr][sc]!=source){
+        return ;
     }
     
+         
+         image[sr][sc]=newColor;
+         
+         helper(image,source,sr-1,sc,newColor,n,m);
+         helper(image,source,sr,sc-1,newColor,n,m);
+         helper(image,source, sr+1,sc,newColor,n,m);                     
+         helper(image,source,sr,sc+1,newColor,n,m);
+        
+        return ;
+        
+    }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
         
-       
-        
-        int n=image.size();
-        int m=image[0].size();
-        int source=image[sr][sc];
-       
-         vector<vector<bool>> visited(n,vector<bool>(m,false));
-         
-        
-        helper(image,sr,sc,newColor,n,m,source,visited);
+        helper(image,image[sr][sc],sr,sc,newColor,image.size(),image[0].size());
         
         return image;
-        
     }
 };
