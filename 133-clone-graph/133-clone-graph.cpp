e46d342d5f075ff44caee5b2ed59aed45aae6ae7
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-11 08:30:29 +0530

    Time: 3 ms (97.08%), Space: 8.7 MB (72.29%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,36 +21,32 @@ public:
 
 class Solution {
 public:
-    void dfs(Node*copy, Node*node,vector<Node*>&vis){
-        vis[copy->val]=copy;
-        
+    void dfs(Node* node,Node* check,vector<Node*>&vis){
+        vis[check->val]=check;
         for(auto j:node->neighbors){
             if(vis[j->val]==NULL){
-                Node* n1=new Node(j->val);
-                dfs(n1,j,vis);
-                 copy->neighbors.push_back(n1);
+                Node*newNode=new Node(j->val);
+                dfs(j,newNode,vis);
+                check->neighbors.push_back(newNode);
             }else{
-                copy->neighbors.push_back(vis[j->val]);
+                
+                check->neighbors.push_back(vis[j->val]);
             }
-            
-            
         }
+        
         
     }
     
     Node* cloneGraph(Node* node) {
         if(node==NULL){
             return NULL;
-        }        
+        }
+        
+        Node*check=new Node(node->val);
         
         vector<Node*>vis(101,NULL);
-        
-        Node* copy=new Node(node->val);
-        
-        dfs(copy,node,vis);
-        
-        return copy;
-        
-        
+        dfs(node,check,vis);
+            
+            return check;
     }
 };
