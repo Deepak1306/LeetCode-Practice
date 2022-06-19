e3987f02dffe2a1d84dd5f867bb2b7c0790e8cac
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-19 20:00:52 +0530

    Added solution - LeetHub

--- Word Break - GFG/word-break.cpp
+++ Word Break - GFG/word-break.cpp
@@ -17,32 +17,36 @@ public:
     int wordBreak(string A, vector<string> &B) {
         //code here
         unordered_map<string,int>m;
-        int n=A.length();
-        int dp[n];
-        memset(dp,0,sizeof(dp));
-        
         for(int i=0;i<B.size();i++){
             m[B[i]]=1;
         }
+         int n=A.length();
+          
+          int dp[n];
+          memset(dp,0,sizeof(dp));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
                 string s=A.substr(j,i-j+1);
-               if(m.find(s)!=m.end()){
-                   dp[i]=1;
-                   if(j>0){
-                       dp[i]=dp[i] && dp[j-1];
-                   }
-                   
-                   if(dp[i]==1){
-                       break;
-                   }
-               }
                 
+                if(m.find(s)!=m.end()){
+                 dp[i]=1;
+                 
+                 if(j>0){
+                     dp[i]=dp[i] && dp[j-1];
+                 }
+                 
+                 if(dp[i]==1){
+                     break;
+                 }
+                 
+                }
             }
         }
         
         return dp[n-1];
+        
+        
     }
 };
 
