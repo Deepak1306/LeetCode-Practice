6828d5198a543afbdedcc82d03bc7aa18b518982
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-28 14:36:22 +0530

    Added solution - LeetHub

--- Reach a given score - GFG/reach-a-given-score.cpp
+++ Reach a given score - GFG/reach-a-given-score.cpp
@@ -7,29 +7,29 @@ using namespace std;
  // } Driver Code Ends
 // Complete this function
 long long int count(long long int n)
-{
-	int arr[3]={3,5,10};
-	
-	int dp[4][n+1];
-	
-	for(int i=0;i<=3;i++){
-	    dp[i][0]=1;
-	}
+{    
+    int arr[3]={3,5,10};
+    
+	long long int dp[4][n+1];
 	
 	for(int j=1;j<=n;j++){
 	    dp[0][j]=0;
 	}
 	
+	for(int i=0;i<=3;i++){
+	    dp[i][0]=1;
+	}
 	
 	for(int i=1;i<=3;i++){
 	    for(int j=1;j<=n;j++){
 	        if(arr[i-1]<=j){
-	            dp[i][j]=dp[i][j-arr[i-1]]+dp[i-1][j];
+	            dp[i][j]=dp[i-1][j]+dp[i][j-arr[i-1]];
 	        }else{
 	            dp[i][j]=dp[i-1][j];
 	        }
 	    }
 	}
+	
 	return dp[3][n];
 	
 }
