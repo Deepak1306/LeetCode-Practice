86b8aa0b77e6753bd927018064d1c754d043f22a
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-28 15:54:41 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -7,22 +7,21 @@ using namespace std;
  // } Driver Code Ends
 // User function Template for C++
 int dp[201][201][2];
+
 class Solution{
 public:
-    int solve(int i, int j, string s, int isTrue){
-        int mod=1003;
+    int solve(string s, int i, int j, int check){
         if(i>j){
             return 0;
         }
         
         if(i==j){
-            if(isTrue==1){
+            if(check==1){
                 if(s[i]=='T'){
                     return 1;
                 }else{
                     return 0;
                 }
-                
             }else{
                 if(s[i]=='F'){
                     return 1;
@@ -30,59 +29,55 @@ public:
                     return 0;
                 }
             }
-            
-            
         }
         
-        if(dp[i][j][isTrue]!=-1){
-            return dp[i][j][isTrue];
+        if(dp[i][j][check]!=-1){
+            return dp[i][j][check];
         }
         int ans=0;
-        
         for(int k=i+1;k<j;k+=2){
-            int lt=solve(i,k-1,s,1);
-            int lf=solve(i,k-1,s,0);
-            int rt=solve(k+1,j,s,1);
-            int rf=solve(k+1,j,s,0);
+            int lt=solve(s,i,k-1,1);
+            int lf=solve(s,i,k-1,0);
+            int rt=solve(s,k+1,j,1);
+            int rf=solve(s,k+1,j,0);
             
-            if(s[k]=='^'){
+            if(s[k]=='&'){
+                if(check==true){
+                    ans+=(lt*rt)%1003;
+                }else{
+                    ans+=((lf*rt)%1003+(lf*rf)%1003+(lt*rf)%1003)%1003;   
+                }
                 
-                if(isTrue==1){
-                 ans+=((lt*rf)%mod+(lf*rt)%mod)%mod;   
-                }else{
-                    ans+=((lt*rt)%mod+(lf*rf)%mod)%mod;
-                }
-            }else if(s[k]=='&'){
-                if(isTrue==1){
-                    ans+=((lt*rt)%mod)%mod;
-                }else{
-                    ans+=((lt*rf)%mod+(lf*rt)%mod+(lf*rf)%mod)%mod;
-                }
-            }else if(s[k]=='|'){
-                if(isTrue==1){
-                    ans+=((lf*rt)%mod+(lt*rf)%mod+(lt*rt)%mod)%mod;
-                }else{
-                    ans+=((lf*rf)%mod)%mod;
-                }
             }
+            else if(s[k]=='|'){
+                if(check==true){
+                    ans+=((lt*rf)%1003+(lt*rt)%1003+(lf*rt)%1003)%1003;
+                }else{
+                    ans+=(lf*rf)%1003;
+                }
+                
+            }else if(s[k]=='^'){
+                if(check==true){
+                    ans+=((lt*rf)%1003+(lf*rt)%1003)%1003;
+                }else{
+                    ans+=((lt*rt)%1003+(rf*lf)%1003)%1003;
+                }
+                
+            }
+            
         }
         
-        return dp[i][j][isTrue]=ans%mod;
-        
-        
-        
-        
-        
+        return dp[i][j][check]=ans%1003;
         
     }
+    
+    
 
     int countWays(int N, string S){
         // code here
-        memset(dp,-1,sizeof(dp));
-        
-        return solve(0,N-1,S,1);
-        
-        
+   memset(dp,-1,sizeof(dp));
+   
+   return solve(S,0,N-1,true);
         
     }
 };
