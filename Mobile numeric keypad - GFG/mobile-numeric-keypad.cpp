e8cf5129e27319796c507d59a43b96dc67b78c0f
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-06-28 16:58:35 +0530

    Added solution - LeetHub

--- Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
+++ Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution{
-	
+
+
 	public:
-	
 	long long getCount(int N)
 	{
 		// Your code goes here
-		vector<vector<int>>paths={{0,8}, {1,2,4}, {1,2,3,5}, {2,6,3}, {1,4,5,7}, {2,4,5,6,8}, {3,5,9,6}, {4,7,8}, {7,5,8,0,9}, {8,6,9}};
+		vector<vector<int>>arr={{0, 8},{1, 2, 4},{2, 1, 3, 5},{3, 2, 6},{4, 1, 5, 7},{5, 2, 4, 6, 8},{6, 3, 5, 9},{7, 8, 4},{8, 5, 7, 9, 0},{9, 6, 8}};
+		
 		
 		long long dp[N+1][10];
 		
 		for(int i=0;i<=N;i++){
-		    for(int j=0;j<=9;j++){
+		    for(int j=0;j<10;j++){
 		        if(i==0){
 		            dp[i][j]=0;
 		        }else if(i==1){
 		            dp[i][j]=1;
 		        }else{
 		            dp[i][j]=0;
-		            for(int prev:paths[j]){
+		            for(auto prev:arr[j]){
 		                dp[i][j]+=dp[i-1][prev];
 		            }
 		            
 		        }
-		        
-		        
 		    }
 		}
 		
 		long long ans=0;
-		for(int i=0;i<=9;i++){
+		
+		for(int i=0;i<10;i++){
 		    ans+=dp[N][i];
 		}
 		
