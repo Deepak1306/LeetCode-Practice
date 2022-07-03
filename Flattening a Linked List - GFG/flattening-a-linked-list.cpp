f9b37c317d1281498e531d4a51d2e615ce023134
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-07-03 15:12:18 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -110,55 +110,38 @@ struct Node{
 
 /*  Function which returns the  root of 
     the flattened linked list. */
-Node*merge(Node*head1, Node*head2){
-    if(head1==NULL){
-        return head2;
-    }else if(head2==NULL){
-        return head1;
-    }
-    
-    
-    Node*head=NULL;
-    Node*tail=NULL;
-    
-    while(head1!=NULL && head2!=NULL){
-        if(head1->data<head2->data){
-            if(head==NULL){
-                head=head1;
-                tail=head1;
-            }else{
+    Node* solve(Node* head1, Node*head2){
+        if(head1==NULL){
+            return head2;
+        }else if(head2==NULL){
+            return head1;
+        }
+        
+        Node*head=new Node(0);
+        Node* tail=head;
+        
+        while(head1!=NULL && head2!=NULL){
+            if(head1->data<head2->data){
                 tail->bottom=head1;
                 tail=tail->bottom;
-            }
-            
-            head1=head1->bottom;
-        }else{
-            if(head==NULL){
-                head=head2;
-                tail=head2;
+                head1=head1->bottom;
             }else{
                 tail->bottom=head2;
                 tail=tail->bottom;
+                head2=head2->bottom;
             }
-            
-            head2=head2->bottom;
         }
+        if(head1!=NULL){
+            tail->bottom=head1;
+        }
+        
+        else if(head2!=NULL){
+            tail->bottom=head2;
+        }
+        
+        return head->bottom;
     }
     
-    while(head1!=NULL){
-        tail->bottom=head1;
-        tail=tail->bottom;
-        head1=head1->bottom;
-    }
-    
-    while(head2!=NULL){
-        tail->bottom=head2;
-        tail=tail->bottom;
-        head2=head2->bottom;
-    }
-    return head;
-}
-
 Node *flatten(Node *root)
 {
    // Your code here
@@ -166,10 +149,7 @@ Node *flatten(Node *root)
        return root;
    }
    
-   root->next=flatten(root->next);
-   
-   root= merge(root,root->next);
-   root->next=NULL;
-   return root;
+   Node* x=flatten(root->next);
+   return solve(root,x);
 }
 
