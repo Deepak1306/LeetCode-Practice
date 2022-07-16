1249ed651b4ba2195cdd193a514cf15da85ddf64
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-07-16 09:50:57 +0530

    Time: 23 ms (7.12%), Space: 17.4 MB (9.93%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<int>&nums , set<vector<int>>& ans, vector<int>a,int i){
+        if(i==nums.size()){
+            sort(a.begin(),a.end());
+            ans.insert(a);
+            return ;
+        }
+
+        solve(nums,ans,a,i+1);
+        a.push_back(nums[i]);
+        solve(nums,ans,a,i+1);
+        a.pop_back();
+        
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        set<vector<int>>s;
+        
+        vector<int>a;
+        
+        solve(nums,s,a,0);
+        
+        for(auto it:s){
+            ans.push_back(it);
+        }
+        
+        return ans;
+    }
+};
