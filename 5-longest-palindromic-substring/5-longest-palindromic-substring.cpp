8e24bb2286371f21ddd04bab81ce871f8477ef07
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-07-18 18:22:22 +0530

    Time: 518 ms (27.44%), Space: 26 MB (38.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,61 +1,50 @@
 class Solution {
 public:
-    string longestPalindrome(string A) {
-         int n=A.length();
+    string longestPalindrome(string s) {
         
-        string s=A;
-        reverse(s.begin(),s.end());
-        
-        string str="";
+        int n=s.length();
         
         bool dp[n][n];
         
+        string ans="";
         for(int g=0;g<n;g++){
             for(int i=0,j=g;j<n;i++,j++){
-                
                 if(g==0){
-                   dp[i][j]=true;
+                    dp[i][j]=true;
                     
-                    if(str.length()<j-i+1){
-                        str=A.substr(i,j-i+1);
+                    if(ans.length()<j-i+1){
+                        ans=s.substr(i,j-i+1);
                     }
-                    
                 }else if(g==1){
-                    if(A[i]==A[j]){
-                        dp[i][j]=true; 
+                    if(s[i]==s[j]){
+                        dp[i][j]=true;
                         
-                       if(str.length()<j-i+1){
-                        str=A.substr(i,j-i+1);
-                       }
-                        
+                        if(ans.length()<j-i+1){
+                            ans=s.substr(i,j-i+1);
+                        }
                     }else{
                         dp[i][j]=false;
                     }
+                    
+                    
                     
                 }else{
-                    if(A[i]==A[j]){
-                        dp[i][j]=dp[i+1][j-1]; 
-                        
+                    if(s[i]==s[j]){
+                        dp[i][j]=dp[i+1][j-1];
                         
                         if(dp[i][j]==true){
-                            
-                          if(str.length()<j-i+1){
-                              str=A.substr(i,j-i+1);
-                           }   
+                            if(ans.length()<j-i+1){
+                            ans=s.substr(i,j-i+1);
+                           }     
                         }
-                        
+                       
                     }else{
                         dp[i][j]=false;
                     }
-                    
                 }
-                
-                
-                
             }
         }
+        return ans;
         
-        
-        return str;
     }
 };
