6e536179c9e2ac04a761a383f46f31907016fee8
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-16 15:44:27 +0530

    Time: 34 ms (43.91%), Space: 8.6 MB (44.79%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -10,16 +10,14 @@ public:
         map<char,int>m;
         
         while(i<n){
-            m[s[i]]++;
-            
-            while(m[s[i]]>1){
-                m[s[j]]--;
-                j++;
-                
+            if(m.find(s[i])!=m.end()){
+                j=max(j,m[s[i]]+1);
             }
             
             ans=max(ans,i-j+1);
+            m[s[i]]=i;
             i++;
+            
         }
         
         return ans;
