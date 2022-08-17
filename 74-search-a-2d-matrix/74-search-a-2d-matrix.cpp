4add93ba1fb1cf6a2548c938d221a25c74f17b2f
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-17 22:07:13 +0530

    Time: 12 ms (15.28%), Space: 9.6 MB (5.02%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,22 +4,36 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         
+        if(target<matrix[0][0]){
+            return false;
+        }
         
-        int in=0;
-        for(int i=1;i<n;i++){
-           if(target<matrix[i][0]){
-               break;
-           } 
-            in=i;
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]>target){
+                break;
+            }
+            k=i;
         }
         
         
-        for(int j=0;j<m;j++){
-            if(target==matrix[in][j]){
+        int l=0;
+        int h=m-1;
+        
+        while(l<=h){
+            int mid=l + (h-l)/2;
+            
+            if(matrix[k][mid]==target){
                 return true;
+            }
+            else if(matrix[k][mid]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
             }
         }
         
         return false;
+        
     }
 };
