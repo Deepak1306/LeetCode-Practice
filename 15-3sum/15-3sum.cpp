abbfd297c122cf59095110b485e779212af2a889
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-17 22:21:42 +0530

    Time: 2189 ms (5.00%), Space: 183.8 MB (6.01%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,64 +2,43 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
-       
         vector<vector<int>>ans;
-        
-        if(n<3){
+        if(n<=2){
             return ans;
         }
         
-       set<vector<int>>seen;
         sort(nums.begin(),nums.end());
         
-        for(int i=0;i<n-1;i++){
-           int count= 0-nums[i];
+        set<vector<int>>s;
+        
+        
+        for(int k=0;k<n-2;k++){
+            int i=k+1;
+            int j=n-1;
             
-            int j=i+1;
-            int k=n-1;
-            
-            
-            
-            while(j<k){
-                if(nums[j]+nums[k]<count){
-                    j++;
-                }else if(nums[j]+nums[k]>count){
-                    k--;
+            while(i<j){
+                int sum=nums[i]+nums[j]+nums[k];
+                
+                if(sum==0){
+                    s.insert({nums[k],nums[i],nums[j]});
+                    i++;
+                    j--;
+                }else if(sum>0){
+                    j--;
                 }else{
-                    
-                    if(nums[j]==nums[k]){
-                      seen.insert({nums[i],nums[j],nums[k]});
-                        break;
-                    }else{
-                        
-                        seen.insert({nums[i],nums[j],nums[k]});
-                        
-                        int j1=j+1;
-                        int k1=k-1;
-                        
-                        while(j1<=k1 && nums[j1]==nums[j]){
-                            j1++;
-                        }
-                        while(k1>=j1 && nums[k1]==nums[k]){
-                            k1--;
-                        }
-                        
-                        j=j1;
-                        k=k1;
-                        
-                        
-                    }
-                    
-                 }
-            }  
+                    i++;
+                }
+                
+            }
             
         }
-        for(auto i:seen){
-          ans.push_back(i);    
+        
+        for(auto i:s){
+            ans.push_back(i);
         }
         
         return ans;
         
-                        
+        
     }
 };
