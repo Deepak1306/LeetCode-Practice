c672d2beebae6929b09d71f845b0008312c05d45
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-17 22:58:27 +0530

    Time: 27 ms (35.96%), Space: 13.4 MB (10.54%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-    void setcols(vector<vector<int>>& matrix,int j,int n,vector<vector<bool>>& visited){
-        
-        for(int i=0;i<n;i++){
-            if(matrix[i][j]!=0){
-             visited[i][j]=true; 
-             matrix[i][j]=0;
+    
+    void col(vector<vector<int>>& matrix,int y,vector<vector<bool>>& arr){
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][y]!=0){
+                matrix[i][y]=0;
+                arr[i][y]=true;
             }
         }
         
-        
     }
-    void setrows(vector<vector<int>>& matrix,int i,int m,vector<vector<bool>>& visited){
-        for(int j=0;j<m;j++){
-            
-            if(matrix[i][j]!=0){
-              matrix[i][j]=0;
-            visited[i][j]=true;  
+    
+    void row(vector<vector<int>>& matrix,int x,vector<vector<bool>>& arr){
+        for(int i=0;i<matrix[0].size();i++){
+            if(matrix[x][i]!=0){
+                matrix[x][i]=0;
+                arr[x][i]=true;
             }
-            
         }
-        
-        
     }
-    
-    
     
     void setZeroes(vector<vector<int>>& matrix) {
+        
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
+       vector<vector<bool>>arr(n,vector<bool>(m,false));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && matrix[i][j]==0){
-                    visited[i][j]=true;
-                    setcols(matrix,j,n,visited);
-                    setrows(matrix,i,m,visited);
+                if(!arr[i][j] && matrix[i][j]==0){
+                    row(matrix,i,arr);
+                    col(matrix,j,arr);
                 }
             }
         }
-        
         
     }
 };
