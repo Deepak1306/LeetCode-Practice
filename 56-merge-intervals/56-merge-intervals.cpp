16e7eb505e1510b9acf98a706b85f65519eeb534
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-18 14:55:34 +0530

    Time: 336 ms (5.01%), Space: 48.3 MB (5.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        
-        if(n==1){
-            return intervals;
+    static bool comp(vector<int>a, vector<int>b){
+        if(a[0]==b[0]){
+           return a[1]<b[1];
+        }else{
+            return a[0]<b[0];
         }
-        
-        sort(intervals.begin(),intervals.end());
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),comp);
         
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
         
-        
-        
-        for(int i=1;i<n;i++){
+        for(int i=1;i<intervals.size();i++){
             int k=ans.size()-1;
             
-            if(intervals[i][0]<=ans[k][1] && intervals[i][1]<=ans[k][1]){
-                continue;
-            }else if(intervals[i][0]<=ans[k][1] && intervals[i][1]>ans[k][1]){
+            if(ans[k][1]>=intervals[i][0] && ans[k][1]<=intervals[i][1]){
                 ans[k][1]=intervals[i][1];
-            }else{
-                ans.push_back(intervals[i]);
+            }else if(ans[k][1]>intervals[i][1]){
+                continue;
             }
-             
+            else{
+                ans.push_back({intervals[i][0],intervals[i][1]});
+            }
+            
         }
         
         return ans;
