15ac82e3cb0d73c19c7bb2bc1c88916f9be42dc4
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-08-28 18:19:38 +0530

    Time: 3 ms (82.24%), Space: 8.1 MB (27.17%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>nums,vector<vector<int>>&ans,vector<int>& a,vector<bool>& vis){
+    
+    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int> &a,map<int,int>&m){
         if(a.size()==nums.size()){
             ans.push_back(a);
             return ;
         }
-
-        for(int k=0;k<nums.size();k++){
-            if(vis[k]){
+        
+        for(int j=0;j<nums.size();j++){
+            if(m.find(nums[j])!=m.end()){
                 continue;
             }
-            vis[k]=true;
-            a.push_back(nums[k]);
-            solve(nums,ans,a,vis);
-            vis[k]=false;
+            a.push_back(nums[j]);
+            m[nums[j]]=1;
+            solve(nums,ans,a,m);
             a.pop_back();
-           
+            m.erase(nums[j]);
+            
         }
         
         
@@ -24,10 +25,9 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>a;
-        int n=nums.size();
-        vector<bool>vis(n,false);
-        solve(nums,ans,a,vis);
-        return ans;
+        map<int,int>m;
+        solve(nums,ans,a,m);
         
+        return ans;
     }
 };
