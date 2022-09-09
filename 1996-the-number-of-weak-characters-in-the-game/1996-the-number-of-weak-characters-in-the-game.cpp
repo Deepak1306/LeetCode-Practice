95fd4660d0a234c9102c3d8f44da9106b7eb274b
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-09-09 16:19:23 +0530

    Time: 1567 ms (9.02%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:    
+   static bool comp(vector<int> &a,vector<int> &b){
+       if(a[0]==b[0]){
+           return a[1]<b[1];
+       }
+       
+       return a[0]>b[0];
+   }
+    
     int numberOfWeakCharacters(vector<vector<int>>& arr) {
-        
-        map<int,multiset<int>>m;
         int n=arr.size();
-        for(int i=0;i<n;i++){
-            m[arr[i][0]].insert(arr[i][1]);
-        }
-        
-        
-        vector<pair<int,multiset<int>>>v(m.begin(),m.end());
-        
-        sort(v.begin(),v.end(),greater<pair<int,multiset<int>>>());
-        
-        int mx=0;
-        for(auto it:v[0].second){
-            mx=max(mx,it);
-        }
-        // cout<<mx<<" ";
-        
         int count=0;
-        for(int i=1;i<v.size();i++){
-            
-            for(auto it:v[i].second){
-                if(it>=mx){
-                    mx=it;
-                    continue;
-                }else{
-                    count++;
-                }
-                
-                // cout<<mx<<" ";
-            }
-        }
         
+        sort(arr.begin(),arr.end(),comp);
+        
+        int mx=arr[0][1];
+        int att=arr[0][0];
+        
+        int i=1;
+        
+        while(i<n){
+            
+            if(arr[i][0]!=att){
+                if(arr[i][1]<mx){
+                    count++;
+                }else if(mx<arr[i][1]){
+                    mx=arr[i][1];
+                    att=arr[i][0];
+                }      
+            }else{
+                mx=max(mx,arr[i][1]);
+            }
+            
+            i++;
+        }
         return count;
     }
 };
