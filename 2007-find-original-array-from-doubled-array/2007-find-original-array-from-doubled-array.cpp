fb6972a4b4a3c476c7564b558bef6ce400db5da1
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-09-15 12:46:30 +0530

    Time: 1938 ms (5.07%), Space: 223.9 MB (5.17%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& arr) {
         map<int,int>map;
-        sort(arr.begin(),arr.end());
+        multiset<int>s;
+        
         int n=arr.size();
         for(int i=0;i<n;i++){
             map[arr[i]]++;
+            s.insert(arr[i]);
         }
         
         vector<int>ans;
         
-        for(int i=0;i<n;i++){
-            if(map.find(arr[i])==map.end()){
+        for(auto i:s){
+            if(map.find(i)==map.end()){
                 continue;
             }
             
-            int half=arr[i]*2;
-            map[arr[i]]--;
-            if(map[arr[i]]==0){
-                map.erase(arr[i]);
+            int half=i*2;
+            map[i]--;
+            if(map[i]==0){
+                map.erase(i);
             }
-            
             
             if(map.find(half)==map.end()){
                 return {};
             }else{
             
-                    ans.push_back(arr[i]);
+                    ans.push_back(i);
                     map[half]--;
                     
                     if(map[half]==0){
@@ -36,7 +37,7 @@ public:
                 }
             }
         
-        
+
         return ans;  
     }
 };
