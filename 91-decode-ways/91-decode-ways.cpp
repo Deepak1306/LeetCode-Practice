30315a15455c36820f6a45b66fd048491632d25b
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-10-01 17:36:35 +0530

    Time: 10 ms (11.49%), Space: 7.4 MB (20.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    
-    
-    int numDecodings(string s) {
+    int dp[101];
+    int solve(string s,int i){
+        if(i==s.length()){
+            return 1 ;
+        }
+        if(s[i]=='0'){
+            return 0 ;
+        }else if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=0;
+       ans+= solve(s,i+1);
         
-        int n=s.length();
-        int dp[n+1];
-
-         
-        dp[n]=1;
         
-        for(int i=n-1;i>=0;i--){
+        if(i<s.length()-1 ){
+            int a=s[i]-'0';
+            int b=s[i+1]-'0';
             
-            if(s[i]=='0'){
-                dp[i]=0;
-                continue;
+            a=a*10+b;
+            
+            if(a>=10 && a<=26){
+            ans+=solve(s,i+2);    
             }
             
-            dp[i]=dp[i+1];
-            
-            
-            if(i<n-1 && (s[i]=='1'|| s[i]=='2' && s[i+1]<'7')){
-                dp[i]+=dp[i+2];
-            }            
         }
-        
-         return s[0]=='0'? 0:dp[0];
+        return dp[i]=ans;
+    }
+    
+    int numDecodings(string s) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0);
         
     }
 };
