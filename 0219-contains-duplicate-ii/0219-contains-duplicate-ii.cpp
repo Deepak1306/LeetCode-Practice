e9cdd5c452d1c06534856b308d07e35e6f2a6b54
Deepak <87796410+Deepak1306@users.noreply.github.com>
2022-10-21 12:08:05 +0530

    Time: 298 ms (67.93%), Space: 80.3 MB (23.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -5,18 +5,10 @@ public:
         map<int,int>mp;
         mp[arr[n-1]]=n-1;
         for(int i=n-2;i>=0;i--){
-            if(mp.find(arr[i])!=mp.end()){
-                
-                if(mp[arr[i]]<=i+k){
+            if(mp.find(arr[i])!=mp.end() && mp[arr[i]]<=i+k){
                   return true;
-                }else{
-                    mp[arr[i]]=min(mp[arr[i]],i);
-                }
             }
-            else{
-                mp[arr[i]]=i;
-            }
-            
+            mp[arr[i]]=i;
         }
         
         return false;
